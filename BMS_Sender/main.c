1eab135d9a2d6cf8636691417179cd4213d4c0af
arjunsivakumar07 <77655050+arjunsivakumar07@users.noreply.github.com>
2021-05-31 13:39:06 +0530

    Update main.c

--- BMS_Sender/main.c
+++ BMS_Sender/main.c
@@ -1,1 +1,26 @@
+/* File Details ***********************************************************************************************
+* File Name   :    main.c
+* Author      : Arjun Sivakumar
+* Description : Battery Management System(BMS) Temperature & SOC Data Sends for every 1 Seconds
+* Functions   : main
+* *********************************************************************************************************** */
 
+/* **************************************** Header Files **************************************************** */
+#include"BMS_DataCollect.h"
+#include"BMS_DataSender.h"
+
+/* Function Details *******************************************************************************************
+* Function Name : main
+* Description   : main function to transmit data for every 1 seconds
+* Arguments     : -
+* Returns       : 0
+* *********************************************************************************************************** */
+int main() {
+    
+    for(int i=0;i <= 15;i++)
+    {
+    BMS_SendData(TO_CONSOLE);
+    }
+    
+    return 0;
+}
