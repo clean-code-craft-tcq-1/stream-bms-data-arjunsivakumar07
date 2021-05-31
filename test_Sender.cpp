618f58a2350d00d08683a41e5966436696bffc24
arjunsivakumar07 <77655050+arjunsivakumar07@users.noreply.github.com>
2021-05-31 14:00:21 +0530

    Update test_Sender.cpp

--- test_Sender.cpp
+++ test_Sender.cpp
@@ -54,7 +54,7 @@ TEST_CASE("Test for invalid Random values")
   float random_value = 0;
   float min = 20;
   float max = 10;
-  REQUIRE(random_float(min,max) == 0);
+  REQUIRE(RandomNumGenerator(min,max) == 0);
 }
 
 TEST_CASE("Test for Send BMS data") 
