82b4a9771f856b6350b4d3f14ccfbf9a699d1df7
arjunsivakumar07 <77655050+arjunsivakumar07@users.noreply.github.com>
2021-05-31 13:57:56 +0530

    Update test_Sender.cpp

--- test_Sender.cpp
+++ test_Sender.cpp
@@ -1,6 +1,6 @@
 #define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
 
-#include "catch.hpp"
+#include "test/catch.hpp"
 #include "BMS_Sender/BMS_DataCollect.h"
 #include "BMS_Sender/BMS_DataSender.h"
 #include "stddef.h"
